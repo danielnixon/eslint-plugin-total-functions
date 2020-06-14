2c795e11c917db567c3b74c942fc30aa789bea4b
Daniel Nixon <dan.nixon@gmail.com>
2020-06-14 14:26:11 +1000

    Publish new rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,9 @@
 import noArraySubscript from "./no-array-subscript";
 import noArrayDestructuring from "./no-array-destructuring";
+import noUnsafeTypeAssertion from "./no-unsafe-type-assertion";
 
 export default {
   "no-array-subscript": noArraySubscript,
   "no-array-destructuring": noArrayDestructuring,
+  "no-unsafe-type-assertion": noUnsafeTypeAssertion,
 };
