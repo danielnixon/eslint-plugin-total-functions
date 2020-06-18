99c1bd860b0eff956450f1cec1dbc066bafef7a4
Daniel Nixon <dan.nixon@gmail.com>
2020-06-18 21:46:30 +1000

    Add no-unsafe-assignment rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,9 +1,11 @@
 import noArraySubscript from "./no-array-subscript";
 import noArrayDestructuring from "./no-array-destructuring";
 import noUnsafeTypeAssertion from "./no-unsafe-type-assertion";
+import noUnsafeAssignment from "./no-unsafe-assignment";
 
 export default {
   "no-array-subscript": noArraySubscript,
   "no-array-destructuring": noArrayDestructuring,
   "no-unsafe-type-assertion": noUnsafeTypeAssertion,
+  "no-unsafe-assignment": noUnsafeAssignment,
 };
