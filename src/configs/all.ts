0db28d0b5d0eeb56459f6e6ddbbb639a026909e2
Daniel Nixon <dan.nixon@gmail.com>
2020-08-15 19:03:35 +1000

    Add all config. Fixes #4

---
+++ src/configs/all.ts
@@ -0,0 +1,9 @@
+export = {
+  extends: ["./configs/base"],
+  rules: {
+    "total-functions/no-array-subscript": "error",
+    "total-functions/no-array-destructuring": "error",
+    "total-functions/no-unsafe-type-assertion": "error",
+    "total-functions/no-unsafe-assignment": "error",
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import rules from "./rules";
 
+import all from "./configs/recommended";
 import recommended from "./configs/recommended";
 
 export = {
   rules,
   configs: {
+    all,
     recommended,
   },
 };
