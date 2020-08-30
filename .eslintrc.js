b4257cd95f5be34dbcfda1f32e8cda8f2f67518b
Daniel Nixon <dan.nixon@gmail.com>
2020-08-30 16:06:05 +1000

    Introduce eslint-config-typed-fp

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,50 +6,16 @@ module.exports = {
     sourceType: "module"
   },
   extends: [
-    "eslint:recommended",
+    "typed-fp",
     "plugin:sonarjs/recommended",
-    "plugin:functional/recommended",
-    "plugin:functional/external-recommended",
     "plugin:jest/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "plugin:total-functions/recommended"
-    // TODO replace tslint's no-any and no-unsafe-any
-    // See https://github.com/typescript-eslint/typescript-eslint/issues/791
+    "plugin:prettier/recommended"
   ],
   env: {
     "jest/globals": true,
     es6: true
   },
   plugins: ["jest", "sonarjs", "functional", "@typescript-eslint", "prettier", "total-functions"],
-  rules: {
-    // Additional rules that are not part of `eslint:recommended`.
-    // See https://eslint.org/docs/rules/
-    "no-eval": "error",
-    "no-implied-eval": "error",
-    "no-await-in-loop": "error",
-    "no-new-wrappers": "error",
-    "eqeqeq": "error",
-    "no-caller": "error",
-    "require-unicode-regexp": "error",
-    "no-loss-of-precision": "error",
-    // Make typescript-eslint rules more aggressive.
-    "@typescript-eslint/consistent-type-assertions": ["error", {
-      "assertionStyle": "never"
-    }],
-    "@typescript-eslint/strict-boolean-expressions": ["error", {
-      "allowString": false,
-      "allowNumber": false,
-      "allowNullableObject": false
-    }],
-    // Interfaces encourage OO, types encourage FP.
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    // Require unknown type annotation in catch blocks.
-    "@typescript-eslint/no-implicit-any-catch": "error",
-    // Don't need this given consistent-type-assertions bans type assertions entirely.
-    "total-functions/no-unsafe-type-assertion": 0
-  }
+  rules: {}
 };
