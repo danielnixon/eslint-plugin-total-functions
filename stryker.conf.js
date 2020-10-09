4c83a77c4be3dec80f8f601a21e82bbf3040f08e
Daniel Nixon <dan.nixon@gmail.com>
2020-10-09 11:45:44 +1100

    Update dependencies

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,16 +1,15 @@
-module.exports = function(config) {
-  config.set({
-    mutator: {
-      name: "typescript",
-      // These are only error message strings (for UI/CLI) so only contribute noise to the mutation output & score.
-      excludedMutations: ["StringLiteral"]
-    },
-    packageManager: "yarn",
-    reporters: ["clear-text", "progress", "dashboard"],
-    testRunner: "jest",
-    transpilers: [],
-    coverageAnalysis: "off",
-    tsconfigFile: "tsconfig.json",
-    mutate: ["src/**/*.ts", "!src/**/*.test.ts", "!src/**/index.ts", "!src/configs/*.ts"]
-  });
+module.exports = {
+  mutator: {
+    // These are only error message strings (for UI/CLI) so only contribute noise to the mutation output & score.
+    excludedMutations: ["StringLiteral"]
+  },
+  packageManager: "yarn",
+  reporters: ["clear-text", "progress", "dashboard"],
+  testRunner: "jest",
+  // TODO: https://github.com/stryker-mutator/stryker/issues/2316
+  coverageAnalysis: "off",
+  // TODO
+  // checkers: ["typescript"],
+  tsconfigFile: "tsconfig.json",
+  mutate: ["src/**/*.ts", "!src/**/*.test.ts", "!src/**/index.ts", "!src/configs/*.ts"]
 };
