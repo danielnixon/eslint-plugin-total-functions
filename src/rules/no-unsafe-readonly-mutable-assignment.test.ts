danielnixon/eslint-plugin-total-functions

commit 344e2c5dde393421d60a87eb029c4d3b4cc5d8f2

Fix infinite recursion bug. Fixes #192

src/rules/unsafe-assignment-rule.ts
@@ -18,6 +18,7 @@ import {
   getCallSignaturesOfType,
   intersectionTypeParts,
   isObjectType,
+  isUnionOrIntersectionType,
   unionTypeParts,
 } from "tsutils";
 
@@ -245,18 +246,23 @@ export const createNoUnsafeAssignmentRule = (
     seenTypes: TypePairArray
   ): boolean => {
     // eslint-disable-next-line functional/no-conditional-statement
-    if (checker.isArrayType(destinationType)) {
-      // Avoid checking every property for unsafe assignment if the source and destination are arrays.
+    if (
+      checker.isArrayType(destinationType) &&
+      !isUnionOrIntersectionType(destinationType) &&
+      !isUnionOrIntersectionType(sourceType)
+    ) {
+      // For performance reasons we avoid checking every array property for unsafe assignment.
+      //
+      // If one of these arrays is readonly and the other mutable, it will be caught by `isUnsafeIndexAssignment`,
+      // so we needn't compare each individual property.
       //
       // Additionally, the length property of tuples is technically mutable (wtf) so we ignore arrays for this reason too.
       // Observe:
       //   const foo = [] as const;
       //   foo.length = 0;
       //
-      // If one of these is readonly and the other mutable, it will be caught by `isUnsafeIndexAssignment`.
-      //
-      // TODO what about types that include arrays as union or intersection members?
       // TODO what about tuples?
+      // TODO should we ignore other built-in types like strings, symbols and numbers for performance too?
       return false;
     }
 
@@ -313,6 +319,13 @@ export const createNoUnsafeAssignmentRule = (
       return false;
     }
 
+    // TODO https://github.com/danielnixon/eslint-plugin-total-functions/issues/100
+    // eslint-disable-next-line total-functions/no-unsafe-mutable-readonly-assignment
+    const nextSeenTypes: TypePairArray = seenTypes.concat({
+      destinationType: rawDestinationType,
+      sourceType: rawSourceType,
+    });
+
     const typePairs = assignableTypePairs(
       rawDestinationType,
       rawSourceType,
@@ -327,13 +340,6 @@ export const createNoUnsafeAssignmentRule = (
 
     const isUnsafeFunctionAssignment = (typePairs: TypePairArray): boolean =>
       typePairs.some(({ sourceType, destinationType }) => {
-        // TODO https://github.com/danielnixon/eslint-plugin-total-functions/issues/100
-        // eslint-disable-next-line total-functions/no-unsafe-mutable-readonly-assignment
-        const nextSeenTypes: TypePairArray = seenTypes.concat({
-          destinationType,
-          sourceType,
-        });
-
         const sourceCallSignatures = getCallSignaturesOfType(sourceType);
         const destinationCallSignatures = getCallSignaturesOfType(
           destinationType
@@ -417,13 +423,6 @@ export const createNoUnsafeAssignmentRule = (
       objectTypePairs: TypePairArray
     ): boolean =>
       objectTypePairs.some(({ sourceType, destinationType }) => {
-        // TODO https://github.com/danielnixon/eslint-plugin-total-functions/issues/100
-        // eslint-disable-next-line total-functions/no-unsafe-mutable-readonly-assignment
-        const nextSeenTypes: TypePairArray = seenTypes.concat({
-          destinationType,
-          sourceType,
-        });
-
         // This is an unsafe assignment if...
         return (
           // we're not in an infinitely recursive type,

jest.config.js
@@ -10,8 +10,8 @@ module.exports = {
     "global": {
       "branches": 95.56, // TODO 100%
       "functions": 100,
-      "lines": 97.9,
-      "statements": 97.91
+      "lines": 97.89,
+      "statements": 97.9
     }
   }
 }

commit 6723c0ed4f5d765be4fe6f8aa5ef60402349fbb4

add tests for regression introduced by 344e2c5dde393421d60a87eb029c4d3b4cc5d8f2
partially revert the changes, retaining the raw types being added to the nextSeenTypes array

src/rules/unsafe-assignment-rule.ts
@@ -340,6 +340,13 @@ export const createNoUnsafeAssignmentRule = (
 
     const isUnsafeFunctionAssignment = (typePairs: TypePairArray): boolean =>
       typePairs.some(({ sourceType, destinationType }) => {
+        // TODO https://github.com/danielnixon/eslint-plugin-total-functions/issues/100
+        // eslint-disable-next-line total-functions/no-unsafe-mutable-readonly-assignment
+        const nextSeenTypesWithPair: TypePairArray = nextSeenTypes.concat({
+          destinationType,
+          sourceType,
+        });
+
         const sourceCallSignatures = getCallSignaturesOfType(sourceType);
         const destinationCallSignatures = getCallSignaturesOfType(
           destinationType
@@ -385,7 +392,7 @@ export const createNoUnsafeAssignmentRule = (
                     sourceParameterType,
                     destinationParameterType,
                     checker,
-                    nextSeenTypes
+                    nextSeenTypesWithPair
                   );
                 });
             };
@@ -410,7 +417,7 @@ export const createNoUnsafeAssignmentRule = (
                 destinationReturnType,
                 sourceReturnType,
                 checker,
-                nextSeenTypes
+                nextSeenTypesWithPair
               ) ||
                 // or the parameter types of the functions are unsafe assignment.
                 isUnsafeParameterAssignment())
@@ -423,6 +430,13 @@ export const createNoUnsafeAssignmentRule = (
       objectTypePairs: TypePairArray
     ): boolean =>
       objectTypePairs.some(({ sourceType, destinationType }) => {
+        // TODO https://github.com/danielnixon/eslint-plugin-total-functions/issues/100
+        // eslint-disable-next-line total-functions/no-unsafe-mutable-readonly-assignment
+        const nextSeenTypesWithPair: TypePairArray = nextSeenTypes.concat({
+          destinationType,
+          sourceType,
+        });
+
         // This is an unsafe assignment if...
         return (
           // we're not in an infinitely recursive type,
@@ -444,7 +458,7 @@ export const createNoUnsafeAssignmentRule = (
             destinationType,
             sourceType,
             checker,
-            nextSeenTypes
+            nextSeenTypesWithPair
           ) ||
             // unsafe number index assignment, or
             isUnsafeIndexAssignment(
@@ -454,7 +468,7 @@ export const createNoUnsafeAssignmentRule = (
               destinationType,
               sourceType,
               checker,
-              nextSeenTypes
+              nextSeenTypesWithPair
             ) ||
             // unsafe property assignment.
             isUnsafePropertyAssignment(
@@ -463,7 +477,7 @@ export const createNoUnsafeAssignmentRule = (
               destinationType,
               sourceType,
               checker,
-              nextSeenTypes
+              nextSeenTypesWithPair
             ))
         );
       });

src/rules/no-unsafe-readonly-mutable-assignment.test.ts
@@ -505,6 +505,60 @@ ruleTester.run("no-unsafe-readonly-mutable-assignment", rule, {
         }
       `,
     },
+    {
+      filename: "file.ts",
+      code: `
+        interface Foo {
+          foo: 'bar'
+        }
+
+        interface Bar {
+          foo: 'bar'
+        }
+
+        const foo: Recursive<Foo> = 42 as any
+        export default foo as Recursive<Bar>
+
+        type Recursive<P> = P | Nested<P>;
+        type Nested<P> = ReadonlyArray<Recursive<P>>;
+      `,
+    },
+    {
+      filename: "file.ts",
+      code: `
+        interface Foo {
+          foo: 'bar'
+        }
+
+        interface Bar {
+          foo: 'bar'
+        }
+
+        const foo: Recursive<Foo> = 42 as any
+        export default foo as Recursive<Bar>
+
+        type Recursive<P> = P | Nested<P>;
+        type Nested<P> = () => Recursive<P>
+      `,
+    },
+    {
+      filename: "file.ts",
+      code: `
+        interface Foo {
+          foo: 'bar'
+        }
+
+        interface Bar {
+          foo: 'bar'
+        }
+
+        const foo: Recursive<Foo> = 42 as any
+        export default foo as Recursive<Bar>
+
+        type Recursive<P> = P | Nested<P>;
+        type Nested<P> = { baz: Recursive<P> }
+      `,
+    },
   ],
   invalid: [
     /**