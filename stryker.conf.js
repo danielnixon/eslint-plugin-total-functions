160682aff2196c2cc4728129a5440e753c21c408
Daniel Nixon <dan.nixon@gmail.com>
2021-07-08 15:59:19 +1000

    Fix stryker

--- stryker.conf.js
+++ stryker.conf.js
@@ -6,8 +6,7 @@ module.exports = {
   packageManager: "yarn",
   reporters: ["clear-text", "progress", "dashboard"],
   testRunner: "jest",
-  // TODO: https://github.com/stryker-mutator/stryker/issues/2316
-  coverageAnalysis: "off",
+  coverageAnalysis: "perTest",
   checkers: ["typescript"],
   tsconfigFile: "tsconfig.json",
   mutate: ["src/**/*.ts", "!src/**/*.test.ts", "!src/**/index.ts", "!src/configs/*.ts"]
