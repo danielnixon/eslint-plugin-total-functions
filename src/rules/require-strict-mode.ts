042fb9bcaf0caf867c3483387593238472417e8a
Daniel Nixon <danixon@agiledigital.com.au>
2023-03-03 12:15:02 +1100

    Typesafe compiler options

--- src/rules/require-strict-mode.ts
+++ src/rules/require-strict-mode.ts
@@ -1,6 +1,24 @@
 /* eslint-disable functional/prefer-immutable-types */
 import { ESLintUtils } from "@typescript-eslint/utils";
+import { CompilerOptions } from "typescript";
 import { createRule } from "./common";
+
+const messages = {
+  strict: "TypeScript's strict mode is required.",
+  noUncheckedIndexedAccess:
+    "TypeScript's noUncheckedIndexedAccess mode is required.",
+  useUnknownInCatchVariables:
+    "Do not disable the useUnknownInCatchVariables compiler option.",
+  strictFunctionTypes:
+    "Do not disable the strictFunctionTypes compiler option.",
+  strictBindCallApply:
+    "Do not disable the strictBindCallApply compiler option.",
+  strictNullChecks: "Do not disable the strictNullChecks compiler option.",
+  strictPropertyInitialization:
+    "Do not disable the strictPropertyInitialization compiler option.",
+};
+
+type CompilerOption = keyof CompilerOptions & keyof typeof messages;
 
 /**
  * An ESLint rule to enforce TypeScript strict mode.
@@ -14,20 +32,7 @@ const requireStrictMode = createRule({
       description: "Enforces the use of TypeScript's strict mode.",
       recommended: "error",
     },
-    messages: {
-      strict: "TypeScript's strict mode is required.",
-      noUncheckedIndexedAccess:
-        "TypeScript's noUncheckedIndexedAccess mode is required.",
-      useUnknownInCatchVariables:
-        "Do not disable the useUnknownInCatchVariables compiler option.",
-      strictFunctionTypes:
-        "Do not disable the strictFunctionTypes compiler option.",
-      strictBindCallApply:
-        "Do not disable the strictBindCallApply compiler option.",
-      strictNullChecks: "Do not disable the strictNullChecks compiler option.",
-      strictPropertyInitialization:
-        "Do not disable the strictPropertyInitialization compiler option.",
-    },
+    messages: messages,
     schema: [],
   },
   create: (context) => {
@@ -37,8 +42,11 @@ const requireStrictMode = createRule({
     return {
       // eslint-disable-next-line functional/no-return-void
       Program: (node) => {
-        const mustBeEnabled = ["strict", "noUncheckedIndexedAccess"] as const;
-        const mustNotBeDisabled = [
+        const mustBeEnabled: readonly CompilerOption[] = [
+          "strict",
+          "noUncheckedIndexedAccess",
+        ] as const;
+        const mustNotBeDisabled: readonly CompilerOption[] = [
           "strictFunctionTypes",
           "strictBindCallApply",
           "strictNullChecks",
