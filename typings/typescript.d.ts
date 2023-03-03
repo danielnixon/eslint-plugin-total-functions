422da1daaed46669f2003fe5250e8395a40af65b
Daniel Nixon <danixon@agiledigital.com.au>
2023-03-03 14:46:54 +1100

    Add some clues to private API usage

--- typings/typescript.d.ts
+++ typings/typescript.d.ts
@@ -6,7 +6,10 @@ import "typescript";
 
 declare module "typescript" {
   interface TypeChecker {
+    // https://github.com/typescript-eslint/typescript-eslint/blob/09c04fb0d5a5dee0dc266abb2e21aa9a1c489712/packages/type-utils/typings/typescript.d.ts#L7-L14
     isArrayType(type: Type): type is TypeReference;
+    // https://github.com/Microsoft/TypeScript/issues/11728
+    // https://github.com/microsoft/TypeScript/issues/9879
     isTypeAssignableTo(type1: Type, type2: Type): boolean;
   }
 }
