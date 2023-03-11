d6aa80ade7ca1052b579cc34b7dc85105b3d4b4f
Daniel Nixon <danixon@agiledigital.com.au>
2023-03-11 14:20:57 +1100

    new rule no-premature-fp-ts-effects

---
+++ src/rules/no-premature-fp-ts-effects.test.ts
@@ -0,0 +1,82 @@
+import rule from "./no-premature-fp-ts-effects";
+import { AST_NODE_TYPES } from "@typescript-eslint/utils";
+import { ESLintUtils } from "@typescript-eslint/utils";
+
+// eslint-disable-next-line functional/prefer-immutable-types
+const ruleTester = new ESLintUtils.RuleTester({
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.tests.json",
+  },
+  parser: "@typescript-eslint/parser",
+});
+
+// eslint-disable-next-line functional/no-expression-statements
+ruleTester.run("no-premature-fp-ts-effects", rule, {
+  valid: [
+    {
+      filename: "file.ts",
+      code: `
+        const myFunc = () => "hello";
+        const result: string = myFunc();
+      `,
+    },
+    {
+      filename: "file.ts",
+      code: `
+        const myFunc = (s: string) => "hello " + s;
+        const result: string = myFunc("asdf");
+      `,
+    },
+    {
+      filename: "file.ts",
+      code: `
+        export interface Lazy<A> {
+          (): A
+        }
+
+        const lazy: Lazy<string> = () => "hello";
+
+        const lazyResult: string = lazy();
+      `,
+    },
+  ],
+  invalid: [
+    {
+      filename: "file.ts",
+      code: `
+        export interface IO<A> {
+          (): A
+        }
+
+        const io: IO<string> = () => "hello";
+
+        const ioResult: string = io();
+      `,
+      errors: [
+        {
+          messageId: "errorStringGeneric",
+          type: AST_NODE_TYPES.CallExpression,
+        },
+      ],
+    },
+    {
+      filename: "file.ts",
+      code: `
+        export interface Task<A> {
+          (): Promise<A>
+        }
+
+        const task: Task<string> = () => Promise.resolve("hello");
+
+        const taskResult: Promise<string> = task();
+      `,
+      errors: [
+        {
+          messageId: "errorStringGeneric",
+          type: AST_NODE_TYPES.CallExpression,
+        },
+      ],
+    },
+  ],
+} as const);
---
+++ src/rules/no-premature-fp-ts-effects.ts
@@ -0,0 +1,78 @@
+/* eslint-disable functional/prefer-immutable-types */
+import { AST_NODE_TYPES, ESLintUtils } from "@typescript-eslint/utils";
+import { createRule } from "./common";
+
+/**
+ * An ESLint rule to ban interpretation (execution) of fp-ts effects.
+ */
+// eslint-disable-next-line total-functions/no-unsafe-readonly-mutable-assignment
+const noEnums = createRule({
+  name: "no-premature-fp-ts-effects",
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Bans interpretation (execution) of fp-ts effects.",
+      recommended: "error",
+    },
+    messages: {
+      errorStringGeneric:
+        "Ensure you aren't interpreting this fp-ts effect until the very end of your program.",
+    },
+    schema: [],
+  },
+  create: (context) => {
+    const parserServices = ESLintUtils.getParserServices(context);
+    const checker = parserServices.program.getTypeChecker();
+
+    return {
+      // eslint-disable-next-line functional/no-return-void
+      CallExpression: (node) => {
+        // eslint-disable-next-line functional/no-conditional-statements
+        if (node.callee.type !== AST_NODE_TYPES.Identifier) {
+          return;
+        }
+
+        // eslint-disable-next-line functional/no-conditional-statements
+        if (node.arguments.length > 0) {
+          return;
+        }
+
+        const calleeNode = parserServices.esTreeNodeToTSNodeMap.get(
+          node.callee
+        );
+        const calleeType = checker.getTypeAtLocation(calleeNode);
+
+        // Note: `Lazy` deliberately excluded even though it has the same signature as `IO`, its semantics
+        // don't imply impurity like IO.
+        const effectInterfaceNames = [
+          "IO",
+          "IOEither",
+          "IOOption",
+          "ReaderTask",
+          "ReaderTaskEither",
+          "StateReaderTaskEither",
+          "Task",
+          "TaskEither",
+          "TaskOption",
+          "TaskThese",
+        ];
+
+        // eslint-disable-next-line functional/no-conditional-statements
+        if (!effectInterfaceNames.includes(calleeType.symbol.name)) {
+          return;
+        }
+
+        // TODO: don't flag error if we can confirm we are at the program entrypoint.
+
+        // eslint-disable-next-line functional/no-expression-statements
+        context.report({
+          node: node,
+          messageId: "errorStringGeneric",
+        } as const);
+      },
+    };
+  },
+  defaultOptions: [],
+} as const);
+
+export default noEnums;
