b350cbb87f547732e2329c2fdb0e98e230c3c811
Daniel Nixon <danixon@agiledigital.com.au>
2023-03-15 22:09:21 +1100

    remove new rule no-nested-fp-ts-effects from default configs

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -11,7 +11,8 @@ export = {
     "total-functions/no-partial-url-constructor": "error",
     "total-functions/no-partial-division": "error",
     "total-functions/no-partial-string-normalize": "error",
-    "total-functions/no-nested-fp-ts-effects": "error",
+    // Not ready for prime time
+    // "total-functions/no-nested-fp-ts-effects": "error",
     "total-functions/no-premature-fp-ts-effects": "error",
     "total-functions/no-partial-array-reduce": "error",
   },
--- src/configs/fp-ts.ts
+++ src/configs/fp-ts.ts
@@ -1,7 +1,8 @@
 export = {
   extends: ["./configs/base"],
   rules: {
-    "total-functions/no-nested-fp-ts-effects": "error",
+    // Not ready for prime time
+    // "total-functions/no-nested-fp-ts-effects": "error",
     "total-functions/no-premature-fp-ts-effects": "error",
   },
 };
